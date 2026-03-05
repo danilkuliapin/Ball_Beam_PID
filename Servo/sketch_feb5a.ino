08cf29453373369d118e83037f21a9e4bfc30209
danilkuliapin <kuliapin.danil@lll.kpi.ua>
2026-03-05 16:01:38 +0200

    Update sketch_feb5a.ino

--- Servo/sketch_feb5a.ino
+++ Servo/sketch_feb5a.ino
@@ -1,27 +1,57 @@
 #include <ESP32Servo.h>
-#include <Adafruit_GFX.h>
-#include <SH1106.h>
 #include <Wire.h>
 
 Servo myServo;
-int servoPin = 10;
+int servoPin = 21;
 
 void setup() 
 {
-  Serial.begin(11520);
+  Serial.begin(115200);
   delay(5000);
-  //myServo.attach(servoPin);
+  myServo.attach(servoPin);
+  myServo.write(90);
+  //while(1);
 }
 
 int angle = 0;
 int direction = 1;
 
-void loop() 
+int center = 85;
+int max_angle = 5;
+int d_l = 50;
+
+void loop()
 {
-  //angle = random(0, 180);
-  //myServo.write(angle);
-  //Serial.print(angle); Serial.println("*");
-  //delay(500);
+  // 0 → -25
+  for(int i = 0; i >= -max_angle*2; i--)
+  {
+    myServo.write(center + i);
+    Serial.println(center + i);
+    delay(d_l);
+  }
 
+  // -25 → 0
+  for(int i = -max_angle*2; i <= 0; i++)
+  {
+    myServo.write(center + i);
+    Serial.println(center + i);
+    delay(d_l);
+  }
 
+  // 0 → +25
+  for(int i = 0; i <= max_angle; i++)
+  {
+    myServo.write(center + i);
+    Serial.println(center + i);
+    delay(d_l);
+  }
+
+  // +25 → 0
+  for(int i = max_angle; i >= 0; i--)
+  {
+    myServo.write(center + i);
+    Serial.println(center + i);
+    delay(d_l);
+  }
 }
+  
